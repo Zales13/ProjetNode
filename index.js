f467374985ee229cfdf2dce8bd3329614d0f5a0d
Fabrice <fabrice.goncalves2@gmail.com>
2021-03-22 15:46:59 +0100

    ajout commentaires

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//creation des variables
 const express = require("express");
 const app = express();
 const fs = require("fs");
@@ -8,23 +9,20 @@ app.get("/", function (req, res) {
 
 app.get("/video", function (req, res) {
     const { name } = req.query;
-    // Ensure there is a range given for the video
     const range = req.headers.range;
     if (!range) {
         res.status(400).send("Requires Range header");
     }
 
-    // get video stats (about 61MB)
     const videoPath = `${name}`;
     const videoSize = fs.statSync(`${name}`).size;
 
-    // Parse Range
-    // Example: "bytes=32324-"
+
     const CHUNK_SIZE = 10 ** 6; // 1MB
     const start = Number(range.replace(/\D/g, ""));
     const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
 
-    // Create headers
+
     const contentLength = end - start + 1;
     const headers = {
         "Content-Range": `bytes ${start}-${end}/${videoSize}`,
@@ -33,13 +31,12 @@ app.get("/video", function (req, res) {
         "Content-Type": "video/mp4",
     };
 
-    // HTTP Status 206 for Partial Content
+
     res.writeHead(206, headers);
 
-    // create video read stream for this particular chunk
+
     const videoStream = fs.createReadStream(videoPath, { start, end });
 
-    // Stream the video chunk to the client
     videoStream.pipe(res);
 });
 
