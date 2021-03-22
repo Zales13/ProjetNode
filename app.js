4865abbe9b33d1fc918795803663cefb7def9147
Thomas Leydet <tomleydet@icloud.com>
2021-03-22 14:15:58 +0100

    Update app.js
    request to user

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const http = require('http')
-const port = 3000
+const port = 3030
 
 const server = http.createServer(function(req,res) {
-
+    res.write('Hello Node')
+    res.end()
 })
 
 server.listen(port, function(error) {
