1b85471d14d7bbe12b3df1161ca5a4cc929ae9b6
Fabrice <fabrice.goncalves2@gmail.com>
2021-03-22 15:25:11 +0100

    modif texte

--- index.js
+++ index.js
@@ -7,23 +7,20 @@ app.get("/", function (req, res) {
 });
 
 app.get("/video", function (req, res) {
-    // Ensure there is a range given for the video
     const range = req.headers.range;
     if (!range) {
         res.status(400).send("Requires Range header");
     }
 
-    // get video stats (about 61MB)
+    //taille de la video
     const videoPath = "720P_VIDEO_ULTRAHD_120FPS.mp4";
     const videoSize = fs.statSync("720P_VIDEO_ULTRAHD_120FPS.mp4").size;
 
-    // Parse Range
-    // Example: "bytes=32324-"
+ //taille du fichier
     const CHUNK_SIZE = 10 ** 6; // 1MB
     const start = Number(range.replace(/\D/g, ""));
     const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
 
-    // Create headers
     const contentLength = end - start + 1;
     const headers = {
         "Content-Range": `bytes ${start}-${end}/${videoSize}`,
@@ -32,13 +29,9 @@ app.get("/video", function (req, res) {
         "Content-Type": "video/mp4",
     };
 
-    // HTTP Status 206 for Partial Content
     res.writeHead(206, headers);
-
-    // create video read stream for this particular chunk
     const videoStream = fs.createReadStream(videoPath, { start, end });
-
-    // Stream the video chunk to the client
+ //stream la video
     videoStream.pipe(res);
 });
 
